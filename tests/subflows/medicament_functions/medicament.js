e10634fb1732ca3b97bc8e03ed0175d5a825d844
Arthur Machado <thuur51@gmail.com>
2025-02-12 14:34:03 -0300

    first commit

---
+++ tests/subflows/medicament_functions/medicament.js
@@ -0,0 +1,23 @@
+output.medicament = {
+    buttons: {
+        newMedicament: "90%,83%",
+        writeMedicament: "90%,78%",
+        iconNewMedicament: "18%,21%",
+        saveExpirateDate: "Save",
+        saveMedicament: "94%,3%",
+        
+    },
+    fields: {
+        medicamentName: "Product name",
+        expirateDate: "Exp. date",
+        displayName: "50%,43%",
+        description: "50%,56%",
+        dose: "26%,68%",
+        amount: "74%,69%",
+        usageIndications: "50%,83%",
+        comment: "50%,84%",
+    },
+    selectors: {
+        measure: "86%,69%",
+    }
+};
